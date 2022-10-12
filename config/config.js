81eb75a4e973d491cb470aafdcd3f23e6e042255
Alexandre78R <alexandre.renard98@gmail.com>
2022-10-12 16:48:20 +0200

    Suppresion node_module

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     //Clé secret de cryptage password
-    "secret" : "tesskssknqknds",
-    "userBDD" : "dddfsdfds"
+    "secret" : "key-magic",
+    "userBDD" : "mongodb+srv://Alexandre78R:bigbossedu785@cluster0.9fw2fyb.mongodb.net/filmJS?retryWrites=true&w=majority"
 }
